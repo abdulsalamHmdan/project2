cddd64316cbfc9d3ffbe31f8d9d1bce20e0c6be0
abdulsalam hmdan <abdulsalam.hmdan@gmail.com>
2025-02-25 17:45:27 +0300

    notsure

--- myapp/static/index.js
+++ myapp/static/index.js
@@ -2,7 +2,19 @@ document.querySelectorAll(".box").forEach(x => {
     x.setAttribute("value", "0");
     // console.log("x")
     x.addEventListener('click', () => {
-        x.classList.toggle("active")
-        x.setAttribute("value", "1");
+        if (x.classList.contains("active")) {
+            x.classList.remove("active")
+            x.classList.add("notsure")
+            x.setAttribute("value", "-1");
+
+        } else if (x.classList.contains("notsure")) {
+            x.classList.remove("notsure")
+            x.setAttribute("value", "0");
+
+        } else {
+            x.classList.add("active")
+            x.setAttribute("value", "1");
+
+        }
     })
 })
